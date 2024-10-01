1839532d79fddd347b9bf1e6e67fb7afd92b0f4f
Yousef Saeed <uosyph@gmail.com>
2024-10-01 13:19:11 +0300

    Test edge cases

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -45,3 +45,66 @@ def test_create_order(client):
     assert beef.stock == 20 - (0.15 * 2)  # 150g * 2
     assert cheese.stock == 5 - (0.03 * 2)  # 30g * 2
     assert onion.stock == 1 - (0.02 * 2)  # 20g * 2
+
+
+def test_order_product_not_found(client):
+    response = client.post(
+        "/orders",
+        json={"products": [{"product_id": 999, "quantity": 2}]},
+    )
+
+    assert response.status_code == 404
+    assert response.json["error"] == "Product with ID 999 not found."
+
+
+def test_order_with_zero_quantity(client):
+    ingredient1 = Ingredient(name="Beef", stock=20, initial_stock=20)
+    product1 = Product(name="Burger")
+
+    db.session.add_all([ingredient1, product1])
+    db.session.commit()
+    db.session.add(
+        ProductIngredient(
+            product_id=product1.id, ingredient_id=ingredient1.id, amount=0.15
+        )
+    )
+    db.session.commit()
+
+    response = client.post(
+        "/orders",
+        json={"products": [{"product_id": product1.id, "quantity": 0}]},
+    )
+
+    assert response.status_code == 400
+    assert response.json["error"] == "Quantity cannot be less than 1."
+
+
+def test_order_insufficient_stock(client):
+    ingredient1 = Ingredient(
+        name="Beef", stock=0.1, initial_stock=0.1
+    )  # Only 100g in stock
+    product1 = Product(name="Burger")
+
+    db.session.add_all([ingredient1, product1])
+    db.session.commit()
+    db.session.add(
+        ProductIngredient(
+            product_id=product1.id, ingredient_id=ingredient1.id, amount=0.15
+        )
+    )
+    db.session.commit()
+
+    response = client.post(
+        "/orders",
+        json={
+            "products": [
+                {
+                    "product_id": product1.id,
+                    "quantity": 2,  # Ordering 2 Burgers (requires 300g of beef)
+                }
+            ]
+        },
+    )
+
+    assert response.status_code == 400
+    assert response.json["error"] == "Insufficient stock for ingredient Beef."
